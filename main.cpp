1f346e650a4a9167988fda76d7a60dd43ae8085f
David Monteiro <102864199+davidmonteiro03@users.noreply.github.com>
2025-03-15 10:39:15 +0000

    THE MOST ROBUST IRC EVER MADE

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ int main(int argc, char **argv)
 	}
 	catch (std::exception &e)
 	{
-		std::cout << e.what() << std::endl;
+		std::cerr << e.what() << std::endl;
 		return EXIT_FAILURE;
 	}
 	return EXIT_SUCCESS;
